1207b1cbed33924e598b2a5483ceb529e3db6d8f
Qasim Awan <qasimawan19@gmail.com>
2023-06-28 17:57:22 +0100

    latest

--- scrpt.js
+++ scrpt.js
@@ -10,7 +10,7 @@ const image2 = document.querySelector("section img:nth-child(2)");
 const image3 = document.querySelector("section img:nth-child(3)");
 
 let clicks = 0;
-const maxClicksAllowed = 5;
+const maxClicksAllowed = 25;
 
 let allProducts = [];
 
@@ -53,7 +53,7 @@ function renderProducts() {
 }
 
 function handleProductClick(event) {
-  if (event.target === productContainer) {
+  if (event.target === ProductContainer) {
     alert("Please click on an image");
   } else {
     clicks++;
@@ -66,8 +66,8 @@ function handleProductClick(event) {
       }
     }
     if (clicks === maxClicksAllowed) {
-      productContainer.removeEventListener("click", handleProductClick);
-      productContainer.className = "no-voting";
+      ProductContainer.removeEventListener("click", handleProductClick);
+      ProductContainer.className = "no-voting";
       resultsButton.addEventListener("click", renderResults);
       resultsButton.className = "clicks-allowed";
     } else {
@@ -86,27 +86,27 @@ function renderResults() {
   }
 }
 
-const bag = new Product("bag", "images/bag.jpg");
-const banana = new Product("banana", "images/banana.jpg");
-const bathroom = new Product("bathroom", "images/bathroom.jpg");
-const boots = new Product("boots", "images/boots.jpg");
-const breakfast = new Product("breakfast", "images/breakfast.jpg");
-const bubblegum = new Product("bubblegum", "images/bubblegum.jpg");
-const chair = new Product("chair", "images/chair.jpg");
-const cthulhu = new Product("cthulhu", "images/cthulhu.jpg");
-const dog = new Product("dog-duck", "images/dog-duck.jpg");
-const dragon = new Product("dragon", "images/dragon.jpg");
-const pen = new Product("pen", "images/pen.jpg");
-const pet = new Product("pet-sweep", "images/pet-sweep.jpg");
-const scissors = new Product("scissors", "images/scissors.jpg");
-const shark = new Product("shark", "images/shark.jpg");
-const sweep = new Product("sweep", "images/sweep.png");
-const tauntaun = new Product("tauntaun", "images/tauntaun.jpg");
-const unicorn = new Product("unicorn", "images/unicorn.jpg");
-const water = new Product("water-can", "images/water-can.jpg");
-const wine = new Product("wine-glass", "images/wine-glass.jpg");
+//const bag = new Product("bag", "images/bag.jpg");
+//const banana = new Product("banana", "images/banana.jpg");
+//const bathroom = new Product("bathroom", "images/bathroom.jpg");
+//const boots = new Product("boots", "images/boots.jpg");
+//const breakfast = new Product("breakfast", "images/breakfast.jpg");
+//const bubblegum = new Product("bubblegum", "images/bubblegum.jpg");
+//const chair = new Product("chair", "images/chair.jpg");
+//const cthulhu = new Product("cthulhu", "images/cthulhu.jpg");
+//const dog = new Product("dog-duck", "images/dog-duck.jpg");
+//const dragon = new Product("dragon", "images/dragon.jpg");
+//const pen = new Product("pen", "images/pen.jpg");
+//const pet = new Product("pet-sweep", "images/pet-sweep.jpg");
+//const scissors = new Product("scissors", "images/scissors.jpg");
+//const shark = new Product("shark", "images/shark.jpg");
+//const sweep = new Product("sweep", "images/sweep.png");
+//const tauntaun = new Product("tauntaun", "images/tauntaun.jpg");
+//const unicorn = new Product("unicorn", "images/unicorn.jpg");
+//const water = new Product("water-can", "images/water-can.jpg");
+//const wine = new Product("wine-glass", "images/wine-glass.jpg");
 
-renderProducts();
+//renderProducts();
 
 ProductContainer.addEventListener("click", handleProductClick);
 
@@ -152,4 +152,45 @@ function renderChart() {
 
   const productChart = document.getElementById("chart");
   const myChart = new Chart(productChart, config);
+  setLocalStorage();
 }
+
+function setLocalStorage() {
+  localStorage.setItem("products", JSON.stringify(allProducts));
+}
+
+function checkLocalStorage() {
+  const localProducts = JSON.parse(localStorage.getItem("products"));
+  // console.log(localProducts);
+  if (localProducts) {
+    allProducts = localProducts;
+  } else {
+    const bag = new Product("bag", "images/bag.jpg");
+const banana = new Product("banana", "images/banana.jpg");
+const bathroom = new Product("bathroom", "images/bathroom.jpg");
+const boots = new Product("boots", "images/boots.jpg");
+const breakfast = new Product("breakfast", "images/breakfast.jpg");
+const bubblegum = new Product("bubblegum", "images/bubblegum.jpg");
+const chair = new Product("chair", "images/chair.jpg");
+const cthulhu = new Product("cthulhu", "images/cthulhu.jpg");
+const dog = new Product("dog-duck", "images/dog-duck.jpg");
+const dragon = new Product("dragon", "images/dragon.jpg");
+const pen = new Product("pen", "images/pen.jpg");
+const pet = new Product("pet-sweep", "images/pet-sweep.jpg");
+const scissors = new Product("scissors", "images/scissors.jpg");
+const shark = new Product("shark", "images/shark.jpg");
+const sweep = new Product("sweep", "images/sweep.png");
+const tauntaun = new Product("tauntaun", "images/tauntaun.jpg");
+const unicorn = new Product("unicorn", "images/unicorn.jpg");
+const water = new Product("water-can", "images/water-can.jpg");
+const wine = new Product("wine-glass", "images/wine-glass.jpg");
+  }
+}
+
+
+checkLocalStorage();
+renderProducts();
+// create the setLocalStorage function and invoke at the bottom of renderChart()
+// create the checklocalStorage()
+// comment out the new instances and place in the else part of if statement
+// invoke the checkLocalStorage()
